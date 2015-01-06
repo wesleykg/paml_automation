9eacb0d4e0d3497f731ef4ed18fe67c0409b5117
wesleykg <wesley.gerelle@gmail.com>
2015-01-05 19:59:03 -0800

    Converter not working

--- 01_converter.py
+++ 01_converter.py
@@ -1,3 +1,14 @@
 from Bio import AlignIO
 
+## Converts aligned FASTA file into an interleaved relaxed phylip file
 AlignIO.convert("seq1.fasta", "fasta", "seq1.phy", "phylip-relaxed")
+
+
+def chomps(s):
+    return s.rstrip('\n')
+## Adds the 'I' signifier to the file so PAML
+## knows the file is interleaved, not sequential
+with open("seq1.phy", "r+") as foo:
+	data = foo.readlines()
+	chomps(data)
+	data[0] += " I"
