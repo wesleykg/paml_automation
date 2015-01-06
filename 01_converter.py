5c2aa8861e6dee6c379a297c5ebaf19d3cdd2c8b
wesleykg <wesley.gerelle@gmail.com>
2015-01-05 18:58:46 -0800

    Completed prototypical fasta -> phylip converter

---
+++ 01_converter.py
@@ -0,0 +1,3 @@
+from Bio import AlignIO
+
+AlignIO.convert("seq1.fasta", "fasta", "seq1.phy", "phylip-relaxed")
--- converter.py
+++
@@ -1,3 +0,0 @@
-from Bio import SeqIO
-
-SeqIO.convert("test1.fas", "fasta", "test1.phy", "phylip")
