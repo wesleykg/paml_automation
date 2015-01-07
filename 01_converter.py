71f18aa0f127adbf305fb41c698b1c2c52b9f2dc
wesleykg <wesley.gerelle@gmail.com>
2015-01-06 18:17:20 -0800

    Added to converter script

--- 01_converter.py
+++ 01_converter.py
@@ -3,12 +3,15 @@ from Bio import AlignIO
 ## Converts aligned FASTA file into an interleaved relaxed phylip file
 AlignIO.convert("seq1.fasta", "fasta", "seq1.phy", "phylip-relaxed")
 
-
-def chomps(s):
-    return s.rstrip('\n')
 ## Adds the 'I' signifier to the file so PAML
 ## knows the file is interleaved, not sequential
-with open("seq1.phy", "r+") as foo:
-	data = foo.readlines()
-	chomps(data)
-	data[0] += " I"
+
+with open("seq1.phy", "r+") as file: # Opens the file
+	data = file.readlines() # Reads the file into memory
+
+data[0] = data[0].rstrip("\n") # Removes the newline character from the first line
+data[0] = data[0] + " I\n" # Adds an I and a newline character to the first line
+
+# Writes the above changes into the file
+with open("seq1.phy", "r+") as file:
+	file.writelines( data )
