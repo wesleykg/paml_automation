48466b9c1ef5a1f82f4cf1604a99c6beea392b8b
wesleykg <wesley.gerelle@gmail.com>
2015-02-02 16:48:16 -0800

    Added script for running pal and reading lnL into a file

--- scripts/02_codeml.py
+++
@@ -1,1 +0,0 @@
-from Bio.Phylo.PAML import codeml
---
+++ scripts/null.py
@@ -0,0 +1,39 @@
+from Bio.Phylo.PAML import codeml
+import sys
+
+cml = codeml.Codeml()
+cml.alignment = sys.argv[1]
+cml.tree = "simple.tre"
+cml.out_file = "mlc"
+
+cml.set_options(CodonFreq = 2)
+cml.set_options(NSsites = [2])
+cml.set_options(fix_omega = 1)
+cml.set_options(clock = 0)
+cml.set_options(ncatG = 5)
+cml.set_options(runmode = 0)
+cml.set_options(fix_kappa = 0)
+cml.set_options(fix_alpha = 1)
+cml.set_options(Small_Diff = 5e-08)
+cml.set_options(method = 0)
+cml.set_options(Malpha = 0)
+cml.set_options(RateAncestor = 0)
+cml.set_options(icode = 0)
+cml.set_options(alpha = 0.0)
+cml.set_options(seqtype = 1)
+cml.set_options(omega = 1)
+cml.set_options(getSE = 0)
+cml.set_options(noisy = 9)
+cml.set_options(Mgene = 0)
+cml.set_options(kappa = 2)
+cml.set_options(model = 2)
+cml.set_options(ndata = 1)
+cml.set_options(cleandata = 0)
+cml.set_options(fix_blength = 0)
+
+results = cml.run(verbose = True)
+
+lnL = results["NSsites"][2]["lnL"]
+
+with open("lnL_null", "w+") as file:
+    file.write(str(lnL))
