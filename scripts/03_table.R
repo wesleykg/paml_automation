e50371c14c833529ea70c5e9725bbbf014223597
Wesley Gerelle <wesley.gerelle@gmail.com>
2015-05-25 17:51:43 -0700

    Added code

--- scripts/03_table.R
+++ scripts/03_table.R
@@ -1,1 +1,11 @@
- 
+gene_name <- commandArgs(trailingOnly = TRUE)[1]
+
+path <- commandArgs(trailingOnly = TRUE)[2]
+
+type <- commandArgs(trailingOnly = TRUE)[3]
+
+filename <- paste0(path, "/lnL_", type, "_", gene_name, ".csv")
+
+data_name <- read.csv(filename)
+
+print (data_name)
