2d7421d85540bc2d65d0d785ce38b20416742a64
Wesley Gerelle <wesley.gerelle@gmail.com>
2015-05-26 17:28:07 -0700

    Beginning new script

--- scripts/03_table.R
+++ scripts/03_table.R
@@ -1,11 +1,1 @@
-gene_name <- commandArgs(trailingOnly = TRUE)[1]
-
-path <- commandArgs(trailingOnly = TRUE)[2]
-
-type <- commandArgs(trailingOnly = TRUE)[3]
-
-filename <- paste0(path, "/lnL_", type, "_", gene_name, ".csv")
-
-data_name <- read.csv(filename)
-
-print (data_name)
+read.csv(file = "results.csv", header = FALSE)
