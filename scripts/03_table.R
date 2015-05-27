c25807cb907dbc880b9c7c623d60bb2e7c5af769
Wesley Gerelle <wesley.gerelle@gmail.com>
2015-05-27 09:02:29 -0700

    Added pseudocode of plan

--- scripts/03_table.R
+++ scripts/03_table.R
@@ -3,3 +3,7 @@ lnL_results <- read.csv(file = "results/results.csv",
 												col.names = c("gene", "method", "lnL")
 												)
 
+If data contains "null" and "alternative":
+	mutate() a new column of an LRT between each alternative and null
+	then compare data in that column to chi square distribution 
+	to produce p value
