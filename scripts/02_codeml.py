fb8b7c14af660c41e790cbff8f49b62caf041fa7
Wesley Gerelle <wesley.gerelle@gmail.com>
2015-05-28 12:16:06 -0700

    Finished unified codeml script

--- scripts/02_codeml.py
+++ scripts/02_codeml.py
@@ -1,29 +1,47 @@
 from Bio.Phylo.PAML import codeml
 import sys, os
 
-if sys.argv[3] == "alternative":
-    NSsites = [2]
-    
-
-
-gene_name = sys.argv[1] # Store the gene filename as a string
+gene_file = sys.argv[1] # Store the gene filename as a string
 gene_name = gene_name[:-4] # Removes the .phy suffix from the gene filename
+tree_file = sys.argv[2]
+method = sys.argv[3]
 
 project_dir = os.getcwd() # Absolute path to the project directory
 gene_dir = os.path.join("results", gene_name) # Relative path to the gene directory
 working_dir = os.path.join(project_dir, gene_dir) # Absolute path to the gene directory
 
+if method == "alternative":
+    model = 2
+    NSsites = [2]
+    fix_omega = 0
+elif method == "null":
+    model = 2
+    NSsites = [2]
+    fix_omega = 1
+elif method == "m1":
+    model = 0
+    NSsites = [1]
+    fix_omega = 0
+elif method == "m0":
+    model = 0
+    NSsites = [0]
+    fix_omega = 0
+elif method == "branch":
+    model = 2
+    NSsites = [0]
+    fix_omega = 0
+
 ## Initializes codeml module
 cml = codeml.Codeml()
 cml.working_dir = os.path.join(working_dir, method)
-cml.alignment = os.path.join(working_dir, sys.argv[1])
-cml.tree = os.path.join(project_dir, sys.argv[2])
+cml.alignment = os.path.join(working_dir, gene_file)
+cml.tree = os.path.join(project_dir, tree_file)
 cml.out_file = os.path.join(cml.working_dir, "mlc")
 
 ## Sets codeml options
 cml.set_options(CodonFreq = 2)
-cml.set_options(NSsites = [2])
-cml.set_options(fix_omega = 0)
+cml.set_options(NSsites = NSsites)
+cml.set_options(fix_omega = fix_omega)
 cml.set_options(clock = 0)
 cml.set_options(ncatG = 5)
 cml.set_options(runmode = 0)
@@ -41,7 +59,7 @@ cml.set_options(getSE = 0)
 cml.set_options(noisy = 9)
 cml.set_options(Mgene = 0)
 cml.set_options(kappa = 2)
-cml.set_options(model = 2)
+cml.set_options(model = model)
 cml.set_options(ndata = 1)
 cml.set_options(cleandata = 0)
 cml.set_options(fix_blength = 0)
@@ -50,7 +68,7 @@ cml.set_options(fix_blength = 0)
 results = cml.run(verbose = True)
 
 ## Stores the results_filename
-results_filename = os.path.join(working_dir, "lnL_alternative_" + gene_name + ".csv")
+results_filename = os.path.join(working_dir, "lnL_" + method + "_" + gene_name + ".csv")
 
 with open(results_filename, "w") as file: # Creates the file for writing
     data = gene_name + "," + method + "," + str(results["NSsites"][2]["lnL"]) + '\n' # Adds the data as a second line
