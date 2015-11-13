7b4b17ad32274404fdc3db0d17361d803f6dfcc1
wesleykg <wesley.gerelle@gmail.com>
2015-11-13 08:17:31 -0800

    cleaning up

--- scripts/01_cleaner.py
+++ scripts/01_cleaner.py
@@ -1,18 +1,20 @@
 import re, sys
+from Bio import SeqIO
 
-gene_file = "../genes/simple.fasta" #Temp test file location
+gene_file = sys.argv[1]
 
-def cleaner(gene_file):
-    with open(gene_file, "r") as gene_file: #Opens the file
+## Fix common FASTA file problems
+def fasta_cleaner(gene_file):
+    with open(gene_file, "r+") as gene_file:
         gene_data = gene_file.read() #Reads the file into memory as a string
         
         ##Check for spaces in gene names/identifiers
         if " " in gene_data:
             sys.exit("Can't read files with spaces in gene names/identifiers")
-        
-        ##Check gene is in multiple of threes
-        #if len(gene_data) is not divisible by three:
-        #    sys.exit("Nucleotides not divisible by 3")
+
+        ##Convert '~' to '-'
+        gene_data = gene_data.replace("~", "-")
+
         ##Convert to Unix line endings
         if "\r" in gene_data:
             if "\n" in gene_data:
@@ -20,12 +22,5 @@ def cleaner(gene_file):
             if "\n" not in gene_data:
                 gene_data = gene_data.replace("\r", "\n") #Classic Mac
                 
-        ##Convert stop codons to ambiguity characters
-        ##FASTA-specific
-        gene_data = re.sub("TAA(?=\n>|\n\n|\n\Z)|TAG(?=\n>|\n\n|\n\Z)|TGA(?=\n>|\n\n|\n\Z)", "???", gene_data, flags = re.IGNORECASE) # Replaces stop codons with ???
-        
-        ##Convert '~' to '-'
-        gene_data = gene_data.replace("~", "-")
-
 if __name__ == '__main__':
-    cleaner(sys.argv[1])
+    fasta_cleaner(gene_file)
