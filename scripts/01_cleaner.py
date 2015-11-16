a88bbb02475fe12581c6c64a30e6fddea0209ba5
wesleykg <wesley.gerelle@gmail.com>
2015-11-16 14:40:47 -0800

    removed unnecesary code

--- scripts/01_cleaner.py
+++ scripts/01_cleaner.py
@@ -1,26 +1,18 @@
-import re, sys
-from Bio import SeqIO
+import sys
 
 gene_file = sys.argv[1]
 
-## Fix common FASTA file problems
-def fasta_cleaner(gene_file):
-    with open(gene_file, "r+") as gene_file:
-        gene_data = gene_file.read() #Reads the file into memory as a string
-        
+def fasta_checker(gene_file):
+    '''Report common FASTA file problems'''
+    with open(gene_file, "r") as gene_file:
         ##Check for spaces in gene names/identifiers
-        if " " in gene_data:
+        if " " in gene_file:
             sys.exit("Can't read files with spaces in gene names/identifiers")
+            
+        ##Check for '~' in place of '-'
+        if '~' in gene_file:
+            sys.exit("Replace '~' chracters with '-' characters")
 
-        ##Convert '~' to '-'
-        gene_data = gene_data.replace("~", "-")
-
-        ##Convert to Unix line endings
-        if "\r" in gene_data:
-            if "\n" in gene_data:
-                gene_data = gene_data.replace("\r\n", "\n") #Windows
-            if "\n" not in gene_data:
-                gene_data = gene_data.replace("\r", "\n") #Classic Mac
-                
+     
 if __name__ == '__main__':
-    fasta_cleaner(gene_file)
+    fasta_checker(gene_file)
