dc93c2b8d8dcc4266e8881c4fa296034ed478e0b
wesleykg <wesley.gerelle@gmail.com>
2015-11-16 15:33:58 -0800

    file initializer added

--- scripts/01_cleaner.py
+++ scripts/01_cleaner.py
@@ -2,9 +2,10 @@ import sys
 from Bio import AlignIO
 
 alignment_file = sys.argv[1]
+in_filetype = 'fasta'
 
 def fasta_check_python(alignment_file):
-    '''Report common FASTA file problems using python libraries'''
+    '''Report common FASTA file problems using base python libraries'''
     with open(alignment_file, "r") as alignment:
         for line in alignment:
             
@@ -17,9 +18,12 @@ def fasta_check_python(alignment_file):
             if '~' in line:
                 sys.exit("Please replace '~' chracters with '-' characters.")
 
-def fasta_check_biopython(alignment_file):
+def fasta_check_biopython(alignment_file, in_filetype):
     '''Report common FASTA file problems using Biopython libraries'''
+    alignment = AlignIO.read(alignment_file, in_filetype)
+    print(alignment)
     
 
 if __name__ == '__main__':
     fasta_check_python(alignment_file)
+    fasta_check_biopython(alignment_file, in_filetype)
