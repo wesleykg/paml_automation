8199fee8b4d5a18da56abfaa4dd1c8a1d9220743
wesleykg <wesley.gerelle@gmail.com>
2016-07-04 16:06:56 -0700

    updating comments for pep8

--- scripts/01_converter.py
+++ scripts/01_converter.py
@@ -1,57 +1,59 @@
 import argparse
 from Bio import AlignIO
 
-#argparse module
-parser = argparse.ArgumentParser() #Initializes argparse
-parser.add_argument('alignment_path', help = 'Path to the alignment file')
-parser.add_argument('out_phyname', help = 'Name for the phylip file')
-parser.add_argument('-f', '--filetype', help = 'Specify the filetype', 
-                    default = 'fasta')
-args, unknown = parser.parse_known_args() #Read valid arguments into 'args'
+# argparse module
+parser = argparse.ArgumentParser()  # Initializes argparse
+parser.add_argument('alignment_path', help='Path to the alignment file')
+parser.add_argument('out_phyname', help='Name for the phylip file')
+parser.add_argument('-f', '--filetype', help='Specify the filetype',
+                    default='fasta')
+args, unknown = parser.parse_known_args()  # Read valid arguments into 'args'
 
-#Set command-line arguments to variables
+# Set command-line arguments to variables
 in_alignment_file = args.alignment_path
 in_filetype = args.filetype
 
 out_phy_file = args.out_phyname
 out_filetype = 'phylip-relaxed'
 
+
 def file_check(in_alignment_file, in_filetype):
     '''Check for common FASTA file problems'''
-    
-    #Base Python
+
+    # Check for problems using Base Python
     with open(in_alignment_file, 'r') as alignment:
         for line in alignment:
-            
-            #Check for spaces in gene names/identifiers
+
+            # Check for spaces in gene names/identifiers
             assert (" " not in line), 'Spaces in gene names/identifiers'
-    
-    #Biopython
+
+    # Check for problems using Biopython
     alignment = AlignIO.read(in_alignment_file, in_filetype)
     for record in alignment:
-        
-        #Checks that the genes are in reading frame for PAML (multiples of 3)
+
+        # Checks that the genes are in reading frame for PAML (multiples of 3)
         assert (len(record.seq) % 3 == 0), 'Incorrect reading frame for PAML'
-        
-        #Checks that genes don't end with stop codons
-        ##KNOWN BUG: CAN"T DEAL WITH LOWERCASE
-        ##FIX: Translate first, then check for '*' characters
-        assert (not record.seq.endswith(('TAA', 
-                                        'TGA', 
-                                        'TAG')) \
-                                        ), 'Stop codon(s) present'
+
+        # Checks that genes don't end with stop codons
+        # KNOWN BUG: CAN"T DEAL WITH LOWERCASE
+        # FIX: Translate first, then check for '*' characters
+        assert (not record.seq.endswith(('TAA', 'TGA', 'TAG'))), \
+            'Stop codon(s) present'
+
 
 def converter(in_alignment_file, in_filetype, out_phy_file, out_filetype):
     '''Convert the alignment into phylip format for PAML'''
+
+    # Converts the file
     AlignIO.convert(in_alignment_file, in_filetype, out_phy_file, out_filetype)
 
-    #Adds an 'I' character to the first line for PAML    
+    # Adds an 'I' character to the first line for PAML
     with open(out_phy_file, "r+") as phy_file:
-        alignment = phy_file.readlines() #List comprising each line
-        alignment[0] = alignment[0].rstrip("\r\n") #Remove newline from line 1
-        alignment[0] = alignment[0] + " I\n" #Add 'I' and newline
-        phy_file.seek(0) #Go back to the start of the file
-        phy_file.writelines(alignment) #Rewrite the file
+        alignment = phy_file.readlines()  # List comprising each line
+        alignment[0] = alignment[0].rstrip("\r\n")  # Remove first newline
+        alignment[0] = alignment[0] + " I\n"  # Add 'I' and newline
+        phy_file.seek(0)  # Go back to the start of the file
+        phy_file.writelines(alignment)  # Rewrite the file
 
 if __name__ == '__main__':
     file_check(in_alignment_file, in_filetype)
--- scripts/02_codeml.py
+++ scripts/02_codeml.py
@@ -1,33 +1,34 @@
 from Bio.Phylo.PAML import codeml
-import os, argparse
+import os
+import argparse
 
-#argparse module
-parser = argparse.ArgumentParser() #Initializes argparse
-parser.add_argument('alignment_path', help = 'Path to the alignment file')
-parser.add_argument('tree_path', help = 'Path to the tree file')
-parser.add_argument('model', help = 'Specify which model to test')
-parser.add_argument('-c', '--cleandata', help = 'Ignore gap-columns', 
-                    default = 0)
-parser.add_argument('-f', '--fix_blength', help = '''Modify initial 
-                    likelihood estimate''', default = 0)
-parser.add_argument('-SE', '--getSE', help = 'Test for standard errors', 
-                    default = 0)
-parser.add_argument('-a', '--RateAncestor', help = '''Test for rates and 
-                    ancestral sequences''', default = 0)
-parser.add_argument('-i', '--icode', help = '''Specify the genetic code for 
-                    ancestral state''', default = 0)
-args, unknown = parser.parse_known_args() #Read valid arguments into 'args'
+# argparse module
+parser = argparse.ArgumentParser()  # Initializes argparse
+parser.add_argument('alignment_path', help='Path to the alignment file')
+parser.add_argument('tree_path', help='Path to the tree file')
+parser.add_argument('model', help='Specify which model to test')
+parser.add_argument('-c', '--cleandata', help='Ignore gap-columns',
+                    default=0)
+parser.add_argument('-f', '--fix_blength', help='''Modify initial
+                    likelihood estimate''', default=0)
+parser.add_argument('-SE', '--getSE', help='Test for standard errors',
+                    default=0)
+parser.add_argument('-a', '--RateAncestor', help='''Test for rates and
+                    ancestral sequences''', default=0)
+parser.add_argument('-i', '--icode', help='''Specify the genetic code for
+                    ancestral state''', default=0)
+args, unknown = parser.parse_known_args()  # Read valid arguments into 'args'
 
-##Set command-line arguments to variables
-#Alignment
+# Set command-line arguments to variables:
+# 1. Alignment
 alignment_file = args.alignment_path
-gene_name = alignment_file[:-4] #Filename without .phy suffix
+gene_name = alignment_file[:-4]  # Filename without .phy suffix
 alignment_dir = os.path.join('results/' + gene_name)
 
-#Tree
+# 2. Tree
 tree_file = args.tree_path
 
-#Method
+# 3. Method
 method = args.model
 if method == "alternative":
     model = 2
@@ -50,55 +51,55 @@ elif method == "nratios":
     NSsites = [0]
     fix_omega = 0
 
-#Optionals
+# Optionals
 cleandata = args.cleandata
 fix_blength = args.fix_blength
 getSE = args.getSE
 RateAncestor = args.RateAncestor
 icode = args.icode
 
-#codeml module
+# codeml module
 cml = codeml.Codeml()
 cml.working_dir = os.path.join(alignment_dir, method)
 cml.alignment = os.path.join(alignment_dir, alignment_file)
 cml.tree = tree_file
 cml.out_file = os.path.join(cml.working_dir, "mlc")
 
-##Sets codeml options
-#Model-specific
-cml.set_options(model = model)
-cml.set_options(NSsites = NSsites)
-cml.set_options(fix_omega = fix_omega)
+# Sets codeml options:
+# 1.Model-specific
+cml.set_options(model=model)
+cml.set_options(NSsites=NSsites)
+cml.set_options(fix_omega=fix_omega)
 
-#Optional settings
-cml.set_options(cleandata = cleandata)
-cml.set_options(fix_blength = fix_blength)
-cml.set_options(getSE = getSE)
-cml.set_options(RateAncestor = RateAncestor)
-cml.set_options(icode = icode)
+# 2. Optional settings
+cml.set_options(cleandata=cleandata)
+cml.set_options(fix_blength=fix_blength)
+cml.set_options(getSE=getSE)
+cml.set_options(RateAncestor=RateAncestor)
+cml.set_options(icode=icode)
 
-#Permanent settings
-cml.set_options(CodonFreq = 2)
-cml.set_options(clock = 0)
-cml.set_options(ncatG = 5)
-cml.set_options(runmode = 0)
-cml.set_options(fix_kappa = 0)
-cml.set_options(fix_alpha = 1)
-cml.set_options(Small_Diff = 5e-08)
-cml.set_options(method = 0)
-cml.set_options(Malpha = 0)
-cml.set_options(alpha = 0.0)
-cml.set_options(seqtype = 1)
-cml.set_options(omega = 1)
-cml.set_options(noisy = 9)
-cml.set_options(Mgene = 0)
-cml.set_options(kappa = 2)
-cml.set_options(ndata = 1)
+# 3. Permanent settings
+cml.set_options(CodonFreq=2)
+cml.set_options(clock=0)
+cml.set_options(ncatG=5)
+cml.set_options(runmode=0)
+cml.set_options(fix_kappa=0)
+cml.set_options(fix_alpha=1)
+cml.set_options(Small_Diff=5e-08)
+cml.set_options(method=0)
+cml.set_options(Malpha=0)
+cml.set_options(alpha=0.0)
+cml.set_options(seqtype=1)
+cml.set_options(omega=1)
+cml.set_options(noisy=9)
+cml.set_options(Mgene=0)
+cml.set_options(kappa=2)
+cml.set_options(ndata=1)
 
-#Runs codeml and stores the results
-results = cml.run(verbose = True)
+# Runs codeml and stores the results
+results = cml.run(verbose=True)
 
-#Retrieves results
+# Retrieve results
 NSsites_dict = results.get('NSsites')
 if method == "alternative":
     model_dict = NSsites_dict.get(2)
@@ -118,7 +119,7 @@ elif method == "m0":
     param_dict = model_dict.get('parameters')
     bg_omega_value = param_dict.get('omega')
     codeml_data = gene_name + ',' + method + ',' + str(lnL_value) + ',' + \
-                    str(bg_omega_value) + '\n'
+        str(bg_omega_value) + '\n'
 elif method == "nratios":
     model_dict = NSsites_dict.get(0)
     lnL_value = model_dict.get('lnL')
@@ -127,9 +128,9 @@ elif method == "nratios":
     bg_omega_value = omega_values[0]
     fg_omega_value = omega_values[1]
     codeml_data = gene_name + ',' + method + ',' + str(lnL_value) + ',' + \
-    str(bg_omega_value) + ',' + str(fg_omega_value) + '\n'
+        str(bg_omega_value) + ',' + str(fg_omega_value) + '\n'
 
-results_filename = os.path.join(alignment_dir, method + 
+results_filename = os.path.join(alignment_dir, method +
                                 "_" + gene_name + ".csv")
 
 with open(results_filename, "w") as lnL_file:
