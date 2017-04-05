bfef71f529d3ca32f9b6f84f68aa8b10993ce994
wesleykg <wesley.gerelle@gmail.com>
2017-04-05 15:02:31 -0700

    now writes significance results to file

--- scripts/03_table.R
+++ scripts/03_table.R
@@ -1,28 +1,30 @@
 library(tidyr)
 
-paml_results.dat <- read.csv(file = '../results/results.csv', header = FALSE, 
+paml_results_dat <- read.csv(file = '../results/results.csv', header = FALSE, 
                        col.names = c(
                          'gene', 'method','lnL')
                        )
 
-paml_results.dat <- spread(paml_results.dat, method, lnL)
+paml_results_dat <- spread(paml_results_dat, method, lnL)
 
-paml_results.dat$BS2_LRT <- (paml_results.dat$alternative - 
-                               paml_results.dat$null)*2
+paml_results_dat$BS2_LRT <- (paml_results_dat$alternative - 
+                               paml_results_dat$null)*2
 
-paml_results.dat$nratios_LRT <- (paml_results.dat$nratios - 
-                               paml_results.dat$m0)*2
+paml_results_dat$nratios_LRT <- (paml_results_dat$nratios - 
+                               paml_results_dat$m0)*2
 
 chisq_1df <- qchisq(.95, df = 1)
 
-if (paml_results.dat$BS2_LRT > chisq_1df) {
-  paml_results.dat$BS2_result = 'Significant'
+if (paml_results_dat$BS2_LRT > chisq_1df) {
+  paml_results_dat$BS2_result = 'Significant'
 } else {
-  paml_results.dat$BS2_result = 'Not significant'
+  paml_results_dat$BS2_result = 'Not significant'
 }
 
-if (paml_results.dat$nratios_LRT > chisq_1df) {
-  paml_results.dat$nratios_result = 'Significant'
+if (paml_results_dat$nratios_LRT > chisq_1df) {
+  paml_results_dat$nratios_result = 'Significant'
 } else {
-  paml_results.dat$nratios_result = 'Not significant'
+  paml_results_dat$nratios_result = 'Not significant'
 }
+
+write.csv(paml_results_dat, file = '../results/results_significant.csv')
