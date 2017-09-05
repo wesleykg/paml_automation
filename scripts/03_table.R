79b739cf199be3bffd07fb19bdaff0f4511f8dc4
wesleykg <wesley.gerelle@gmail.com>
2017-09-05 16:01:13 -0700

    added if statements for different LRT tests

--- scripts/03_table.R
+++ scripts/03_table.R
@@ -21,25 +21,34 @@ LRTdat <- paml_results_dat %>% select(gene, method, lnL)
 LRTdat <- spread(LRTdat, method, lnL)
 
 ## Record LRT result for each test type
-LRTdat$BS2_LRT <- (LRTdat$alternative - LRTdat$null)*2
-LRTdat$branch_LRT <- (LRTdat$nratios - LRTdat$m0)*2
-
+if ("alternative" %in% colnames(LRTdat)) {
+  LRTdat$BS2_LRT <- (LRTdat$null - LRTdat$alternative)*-2
+}
+if ("nratios" %in% colnames(LRTdat)){
+  LRTdat$branch_LRT <- (LRTdat$m0 - LRTdat$nratios)*-2
+}
+if ("CmC" %in% colnames(LRTdat)){
+  LRTdat$CmC_LRT <- (LRTdat$m2a_rel - LRTdat$CmC)*-2
+}
+if ("CmD" %in% colnames(LRTdat)){
+  LRTdat$CmD_LRT <- (LRTdat$m3 - LRTdat$CmC)*-2
+}
 ## Record chi^2 significance threshold for later tests
 chisq_1df <- qchisq(.95, df = 1)
 
 ## Compare LRT to chi^2 value to test for significance. Write the significance
 ## result into the table
-if (LRTdat$BS2_LRT > chisq_1df) {
-  LRTdat$BS2_result = 'Significant'
-} else {
-  LRTdat$BS2_result = 'Not significant'
-}
-
-if (LRTdat$branch_LRT > chisq_1df) {
-  LRTdat$branch_result = 'Significant'
-} else {
-  LRTdat$branch_result = 'Not significant'
-}
+# if (LRTdat$BS2_LRT > chisq_1df) {
+#   LRTdat$BS2_result = 'Significant'
+# } else {
+#   LRTdat$BS2_result = 'Not significant'
+# }
+# 
+# if (LRTdat$branch_LRT > chisq_1df) {
+#   LRTdat$branch_result = 'Significant'
+# } else {
+#   LRTdat$branch_result = 'Not significant'
+# }
 
 ## Write significance results to file
-write.csv(LRTdat, file = 'results/results_significant.csv')
+write.csv(LRTdat, file = 'results/results_LRT.csv')
